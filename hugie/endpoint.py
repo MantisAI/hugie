93b222fe2e6323de86e0bd7f84aca4b8a7ebc548
Tarun Samanta <tarunsamanta77@gmail.com>
2023-07-24 00:04:23 +0530

    refactored the list endpoints function as per your suggestions

--- hugie/endpoint.py
+++ hugie/endpoint.py
@@ -40,31 +40,39 @@ def list(
 
     try:
         response = requests.get(
-            f"{settings.endpoint_url}", headers=headers, json={}
+            f"{settings.endpoint_url}", headers=headers, 
         )
+        response.raise_for_status()
+        
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 401:
+            typer.secho("Invalid authentication credentials or token", fg=typer.colors.YELLOW)
+        elif e.response.status_code == 500:
+            typer.secho("Internal Server Error", fg=typer.colors.YELLOW)
+        elif e.response.status_code == 501:
+            typer.secho("Not Implemented", fg=typer.colors.YELLOW)
+        elif e.response.status_code == 502:
+            typer.secho("Bad Gateway", fg=typer.colors.YELLOW)
+        elif e.response.status_code == 503:
+            typer.secho("Service Unavailable", fg=typer.colors.YELLOW)
+        elif e.response.status_code == 504:
+            typer.secho("Gateway Timeout", fg=typer.colors.YELLOW)
+        elif e.response.status_code == 509:
+            typer.secho("Bandwidth Limit Exceeded", fg=typer.colors.YELLOW)    
+        elif e.response.status_code == 511:
+            typer.secho("Network Authentication Required", fg=typer.colors.YELLOW)
+            
+        else:
+            typer.secho("Error creating endpoint", fg=typer.colors.RED)
+        raise SystemExit(e)
     except requests.exceptions.RequestException as e:
         typer.secho(API_ERROR_MESSAGE, fg=typer.colors.RED)
-        response.raise_for_status()
         raise SystemExit(e)
-    
-    if response.status_code == 401:
-        typer.secho(f"Invalid authentication credentials or token", fg=typer.colors.YELLOW)
-    elif response.status_code == 500:
-        typer.secho(f"Internal Server Error", fg=typer.colors.YELLOW)
-    elif response.status_code == 501:
-        typer.secho(f"Not Implemented", fg=typer.colors.YELLOW)
-    elif response.status_code == 502:
-        typer.secho(f"Bad Gateway", fg=typer.colors.YELLOW)
-    elif response.status_code == 503:
-        typer.secho(f"Service Unavailable", fg=typer.colors.YELLOW)
-    elif response.status_code == 504:
-        typer.secho(f"Gateway Timeout", fg=typer.colors.YELLOW)
-    elif response.status_code == 509:
-        typer.secho(f"Bandwidth Limit Exceeded", fg=typer.colors.YELLOW)    
-    elif response.status_code == 511:
-        typer.secho(f"Network Authentication Required", fg=typer.colors.YELLOW)
     else:
-        typer.secho("Successfully listed all endpoints", fg=typer.colors.GREEN)
+        typer.secho(
+            "Successfully listed all endpoints",
+            fg=typer.colors.GREEN,
+        )
 
     if json:
         return typer.echo(response.json())
